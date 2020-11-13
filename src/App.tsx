39b78119a165541cd56c088a1b9d58d6b27e4a2d
Tom <wonggamto@163.com>
2020-11-14 00:58:43 +0800

    react-router初体验

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,63 @@
 import React from 'react';
-import './App.css';
-
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 function App() {
-  return(
-    <div>hello</div>
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </nav>
 
-  ) ;
+        <Switch>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+          <Redirect exact from="/" to="/money"/>
+        </Switch>
+      </div>
+    </Router>
+
+  );
+}
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Money() {
+  return <h2>Money</h2>;
+}
+
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+function NoMatch() {
+  return <h2>页面不存在</h2>
 }
 
 export default App;
