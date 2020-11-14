cb83e8102ee4010fcd6c989a16aee5784eb922b5
Tom <wonggamto@163.com>
2020-11-15 00:47:44 +0800

    封装Nav组件

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,22 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect
 } from 'react-router-dom';
 import styled from 'styled-components';
-
+import Nav from './components/Nav';
 const Wrapper = styled.div`
     height: 100vh;
     display: flex;
     flex-direction: column;
+;
     
 `;
 const Main = styled.div`
   flex-grow: 1;
   overflow: auto;
 `;
-const Nav = styled.div`
-  border:1px solid blue;
-  > ul{
-      display: flex;
-    > li{
-      width:33.333333%;
-      text-align: center;
-      padding:16px;
-      
-    }
-  }
-`
+
 function App() {
   return (
     <Router>
@@ -51,19 +40,8 @@ function App() {
             <Redirect exact from="/" to="/money"/>
           </Switch>
         </Main>
-        <Nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签页</Link>
-            </li>
-            <li>
-              <Link to="/money">记账</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计</Link>
-            </li>
-          </ul>
-        </Nav>
+        <Nav />
+
       </Wrapper>
     </Router>
 
