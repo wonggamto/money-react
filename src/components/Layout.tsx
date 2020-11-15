c9ed374157facbcf1a92fa1c4e2f823344562e55
Tom <wonggamto@163.com>
2020-11-15 23:59:10 +0800

    创建Layout组件

---
+++ src/components/Layout.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Nav from './Nav';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+;
+    
+`;
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+const Layout = (props: any) => {
+  return (
+    <Wrapper>
+      <Main>
+        <h2>{props.children}</h2>
+      </Main>
+      <Nav/>
+    </Wrapper>
+  );
+};
+export default Layout;
