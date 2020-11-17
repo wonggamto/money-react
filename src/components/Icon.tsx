9164e0cfe0155fe0af04e9e369d4d34923d660fe
Tom <wonggamto@163.com>
2020-11-17 09:46:27 +0800

    修复 Nav 点击区域过小的问题

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-// require('icons/money.svg');
-// require('icons/chart.svg');
-// require('icons/tag.svg');
+
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
 type Props = {
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,11 +12,13 @@ const NavWrapper = styled.div`
     > li{
       width:33.333333%;
       text-align: center;
-      padding:4px 0;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center; 
+      > a{
+        padding:4px 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;  
+      }
       .icon{
         width:24px;
         height: 24px;
@@ -29,16 +31,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-         <Icon name="tag"/>
-          <Link to="/tags">标签页</Link>
+          <Link to="/tags">
+            <Icon name="tag"/>
+            标签页
+          </Link>
         </li>
         <li>
-          <Icon name="money"/>
-          <Link to="/money">记账</Link>
+          <Link to="/money">
+            <Icon name="money"/>
+            记账
+          </Link>
         </li>
         <li>
-          <Icon name="chart" />
-          <Link to="/statistics">统计</Link>
+          <Link to="/statistics">
+            <Icon name="chart"/>
+            统计
+          </Link>
         </li>
       </ul>
     </NavWrapper>
