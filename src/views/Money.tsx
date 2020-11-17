6d4942fb7fe673aea60bf6f633153fa3b21c35f5
Tom <wonggamto@163.com>
2020-11-17 15:13:32 +0800

    基本结构完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,10 +1,69 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import styled from 'styled-components'
+import Icon from '../components/Icon';
 
+const TagsSection = styled.section``
+
+const NotesSection = styled.section``
+
+const CategorysSection = styled.section``
+
+const NumberPadSection = styled.section``
 function Money() {
   return (
     <Layout>
-      <h2>记账页</h2>
+      <TagsSection>
+        <ol>
+          <li>
+            <Icon name="gift"/>
+          </li>
+          <li>
+            <Icon name="foods"/>
+          </li>
+          <li>
+            <Icon name="habitation"/>
+          </li>
+          <li>
+            <Icon name="transportation"/>
+          </li>
+        </ol>
+      </TagsSection>
+      <NotesSection>
+        <label>
+          <span>备注:</span>
+          <input type="text"/>
+        </label>
+      </NotesSection>
+      <CategorysSection>
+        <ul>
+          <li>
+            <Icon name="income"/>
+          </li>
+          <li>
+            <Icon name="pay"/>
+          </li>
+        </ul>
+      </CategorysSection>
+      <NumberPadSection>
+        <div>100</div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>OK</button>
+        <button>0</button>
+        <button>.</button>
+      </NumberPadSection>
     </Layout>
   );
 }
