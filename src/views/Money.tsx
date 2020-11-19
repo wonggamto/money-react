6031067edb050ff5b93f33e1f39a723d7e5c5b75
Tom <wonggamto@163.com>
2020-11-19 11:22:39 +0800

    NumberPad UI完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -54,13 +54,16 @@ const NotesSection = styled.section`
 const CategorySection = styled.section`
     font-size: 24px;
     > ul{
-      background: #c4c4c4;
       display: flex;
+      background: #c4c4c4;
       > li{
         width: 50%;
         padding: 16px 0;
         text-align: center;
         position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: center;
         &.selected::after{
             content:'';
             display: block;
@@ -75,9 +78,63 @@ const CategorySection = styled.section`
     }
 `;
 
-const NumberPadSection = styled.section``;
-
-
+const NumberPadSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  > .output{
+    background: white;
+    font-size:36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
+                inset 0 5px 5px -5px rgba(0,0,0,0.25);
+  }
+  > .pad{
+    > button{
+      font-size: 18px;
+      float: left;
+      width: 25%;
+      height: 64px;
+      border: none;
+      &.ok{
+        height: 128px;
+        float: right;
+      }
+      &.zero{
+        width: 50%;
+      }
+      &:nth-child(1){
+        background: #f2f2f2;
+      }
+       &:nth-child(2),
+       &:nth-child(5){
+        background: #E0E0E0;
+      }
+       &:nth-child(3),
+       &:nth-child(6),
+       &:nth-child(9){
+        background: #D3D3D3;
+      }
+      &:nth-child(4),
+       &:nth-child(7),
+       &:nth-child(10){
+        background: #C1C1C1;
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13){
+        background: #B8B8B8;
+      }
+      &:nth-child(12){
+        background: #9A9A9A;
+      }
+      &:nth-child(14){
+        background: #A9A9A9;
+      }
+    }
+  }
+`;
 function Money() {
   return (
     <Layout>
@@ -118,23 +175,23 @@ function Money() {
         </label>
       </NotesSection>
       <NumberPadSection>
-        <div>100</div>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>+</button>
-        <button>删除</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
-        <button>清空</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>OK</button>
-        <button>0</button>
-        <button>.</button>
+        <div className="output ">100</div>
+        <div className="pad clearfix">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
+          <button>.</button>
+        </div>
       </NumberPadSection>
     </Layout>
   );
